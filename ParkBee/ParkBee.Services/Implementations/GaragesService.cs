10a197b14491612d044ea85176c9b3830f95922e
tarekselem <t.hamdy91@yahoo.com>
2018-04-10 00:22:41 +0200

    Implement Services

--- ParkBee/ParkBee.Services/Implementations/GaragesService.cs
+++ ParkBee/ParkBee.Services/Implementations/GaragesService.cs
@@ -2,7 +2,9 @@
 using ParkBee.Services.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using LinqKit;
 using System.Threading.Tasks;
 
 namespace ParkBee.Services.Implementations
@@ -29,22 +31,76 @@ namespace ParkBee.Services.Implementations
 
         bool IGaragesService.Delete(Guid id)
         {
-            throw new NotImplementedException();
+            if (Guid.Empty == id) throw new ArgumentNullException(nameof(id));
+
+            bool result = _unitOfWork.RepositoryFor<Entities.Garage>().Delete(id);
+            if (result) _unitOfWork.SaveChanges();
+
+            return result;
         }
 
         Task<ViewModels.GarageList> IGaragesService.Get(int pageIndex, int pageSize, string keyword)
         {
-            throw new NotImplementedException();
+            var predicate = BuildSearchFilter(keyword);
+
+            //var items = await _unitOfWork.RepositoryFor<Entities.Garage>().GetAsync(garage => new ViewModels.Garage
+            //{
+            //    Id = garage.Id,
+            //    CustomerId = garage.CustomerId,
+            //    CompanyName = customer.CompanyName,
+            //    ContactName = customer.ContactName,
+            //    ContactTile = customer.ContactTile,
+            //    Phone = customer.Phone,
+            //    Fax = customer.Fax,
+            //    PostalCode = customer.PostalCode,
+            //    Address = customer.Address,
+            //    City = customer.City,
+            //    Region = customer.Region,
+            //    Country = customer.Country,
+            //    OrdersCount = customer.Orders.Count
+            //}, predicate, c => c.OrderBy(o => o.Id), null, pageIndex, pageSize);
+            //var total = _unitOfWork.RepositoryFor<Entities.Garage>().Get().Count();
+
+            //return new ViewModels.CustomersList { Items = items, Total = total };
+            return null;
         }
 
-        ViewModels.GarageDetails IGaragesService.GetCustomerById(Guid id)
+        ViewModels.GarageDetails IGaragesService.GetGarageById(Guid id)
         {
-            throw new NotImplementedException();
+            var includedProperties = new List<Expression<Func<Entities.Garage, object>>>() { c => c.Doors };
+            var garageEntity = _unitOfWork.RepositoryFor<Entities.Garage>().Get(filter: c => c.Id == id, includedProperties: includedProperties).FirstOrDefault();
+
+            if (garageEntity == null) return null;
+
+            return AutoMapper.Mapper.Map<ViewModels.GarageDetails>(garageEntity);
         }
 
-        bool IGaragesService.Update(BindingModel.Garage customerBindingModel)
+        bool IGaragesService.Update(BindingModel.Garage garageBindingModel)
         {
-            throw new NotImplementedException();
+            if (garageBindingModel == null) throw new ArgumentNullException(nameof(garageBindingModel));
+
+            var garageEntity = AutoMapper.Mapper.Map<Entities.Garage>(garageBindingModel);
+            bool result = _unitOfWork.RepositoryFor<Entities.Garage>().Update(garageEntity);
+
+            if (result) _unitOfWork.SaveChanges();
+
+            return result;
         }
+
+
+        #region Private Methods
+        private ExpressionStarter<Entities.Garage> BuildSearchFilter(string keyword)
+        {
+            var predicate = PredicateBuilder.True<Entities.Garage>();
+
+            if (!string.IsNullOrEmpty(keyword))
+            {
+                predicate = predicate.And(s => s.Address.ToLower().Contains(keyword.ToLower()));
+                predicate = predicate.And(s => s.Name.ToLower().Contains(keyword.ToLower()));
+            }
+
+            return predicate;
+        }
+        #endregion
     }
 }
--- ParkBee/ParkBee.Services/Interfaces/IGaragesService.cs
+++ ParkBee/ParkBee.Services/Interfaces/IGaragesService.cs
@@ -6,7 +6,7 @@ namespace ParkBee.Services.Interfaces
     public interface IGaragesService
     {
         Task<ViewModels.GarageList> Get(int pageIndex, int pageSize, string keyword = null);
-        ViewModels.GarageDetails GetCustomerById(Guid id);
+        ViewModels.GarageDetails GetGarageById(Guid id);
         ViewModels.Garage Add(BindingModel.Garage garageBindingModel);
         bool Update(BindingModel.Garage garageBindingModel);
         bool Delete(Guid id);
